46878537b7c6dc6609ca5f7dffd7ffee258e0e8b
Nehchal18 <116391659+Nehchal18@users.noreply.github.com>
2024-09-23 23:58:39 +0530

    adding cart page and functionality in signup

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var app = express();
 
 // Serve the 'public' folder for static assets (CSS, JS, images)
 app.use(express.static("public"));
+app.use(express.json());
 
 // Define custom routes for specific pages
 app.get("/product", (req, res) => {
@@ -20,6 +21,33 @@ app.get("/404", (req, res) => {
 
 app.get("/signup", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "signup.html"));
+});
+
+app.post('/signup', (req, res) => {
+    let { name, email, password, number, tac, notification } = req.body;
+
+    // form validations
+    if(name.length < 3){
+        return res.json({'alert': 'name must be 3 letters long'});
+    } else if(!email.length){
+        return res.json({'alert': 'enter your email'});
+    } else if(password.length < 8){
+        return res.json({'alert': 'password should be 8 letters long'});
+    } else if(!number.length){
+        return res.json({'alert': 'enter your phone number'});
+    } else if(!Number(number) || number.length < 10){
+        return res.json({'alert': 'invalid number, please enter valid one'});
+    } else if(!tac){
+        return res.json({'alert': 'you must agree to our terms and conditions'});
+    } else{
+        // submit form
+        return res.json({'alert': 'form submitted successfully'});
+    }
+})
+
+
+app.get("/cart", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "cart.html"));
 });
 
 // Start the server on port 8080
